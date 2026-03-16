c22bbf6c28a5ef369871d7e1b053e8e0db29e8c4
Your Name <you@example.com>
2026-03-16 01:49:13 +0000

    terraform ci

---
+++ Task2Advanced/terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "token" {}
+variable "cloud_id" {}
+variable "folder_id" {}
+variable "zone" {}
+
+variable "subnet_id" {}
+variable "ssh_key" {}
+
+variable "vm_name" {}
+variable "disk_size_gb" {}
+variable "cores" {}
+variable "memory" {}
