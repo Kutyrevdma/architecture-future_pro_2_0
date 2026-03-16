da8cc3507e79f398dad1353edbb8cab3f3708a87
Your Name <you@example.com>
2026-03-16 02:05:28 +0000

    fix terraform working directory_2

--- Task2Advanced/terraform/backend.tf
+++ Task2Advanced/terraform/backend.tf
@@ -9,7 +9,6 @@ terraform {
       s3 = "https://storage.yandexcloud.net"
     }
 
-
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
