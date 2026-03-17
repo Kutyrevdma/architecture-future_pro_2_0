41247c44d9891f14623e1c538d853faf5c0b2e05
Your Name <you@example.com>
2026-03-17 07:51:24 +0000

    test_2

--- Task2Advanced/terraform/backend.tf
+++ Task2Advanced/terraform/backend.tf
@@ -1,18 +1,3 @@
 terraform {
-  backend "s3" {
-
-    bucket = "tf-state-dmitriy"
-    key    = "terraform/terraform.tfstate"
-    region = "ru-central1"
-
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    force_path_style            = true
-  }
+  backend "s3" {}
 }
